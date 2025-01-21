0bc48178808a5466d3321d9b4beb275e4aae949b
iris5105 <iris510593@gmail.com>
2025-01-21 10:49:58 +0900

    Header 뼈대 완성성

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { Layout} from 'antd';
+import { Layout, Typography, Flex} from 'antd';
+import Icon, {UserOutlined} from '@ant-design/icons';
+const { Text, Link, Title  } = Typography;
 
 const Header = () => {
 
   return (
-    <div>
       <Layout
       style={{
         display: 'flex',
-        alignItems: 'center',
-        backgroundColor : '#3f51b5'
+        height : '50px',
+        backgroundColor : '#3f51b5',
+        paddingLeft : '10px',
+        paddingRight : '10px'
       }}
     >
-    <div className="demo-logo" />
-    <h2 italic="true" style={{color : 'yellow'}}>A</h2>
+      <Flex horizontal justify='space-between' align='center'
+      
+      >
+        <Flex horizontal justify='flex-start' align='center'>
+          <div className="demo-logo" />
+          <Title  level = {2} italic style={{color : 'yellow', marginTop : '0'}}>A</Title>
+          <Title  level = {4} style={{color : 'white', marginTop : '0'}}>dvanced Asset Management System.</Title>
+        </Flex>
+        <Flex horizontal justify='flex-start' align='center'>
+          <Text style={{color : 'white'}}>사용자 </Text>
+          <Text style={{color : 'white'}}>님반갑습니다.</Text>
+        </Flex>
+        <Flex horizontal justify='flex-start' align='center'>
+          <UserOutlined style={{ fontSize : '18px',color : 'white'}}/>
+          <Text style={{ fontSize : '18px',color : 'white'}}>Logout</Text>
+        </Flex>
+        
+      </Flex>
     </Layout>
-  </div>
   )
 }
 
