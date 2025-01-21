497fbebe3801502e437f5dbe3c886461842bbb79
iris5105 <iris510593@gmail.com>
2025-01-21 10:50:27 +0900

    테마 토큰 제거 및 전체적인 뼈대 완성

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -1,43 +1,31 @@
 import React, {useState} from 'react';
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Flex, Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Flex, Breadcrumb, Layout, Menu, theme, Typography } from 'antd';
 import Header from './Header.jsx';
 import Sider from './Sider.jsx';
+const {Text } = Typography
 const Main = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
   return (
-    <Flex gap="middle" wrap>
-    <Layout> 
-      <Header
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          backgroundColor : '#3f51b5'
-        }}
-      >
-       <div className="demo-logo" />
-       
-      </Header>
-      <Layout style={{ display: 'flex', flexDirection: 'row' }}>
-      <Sider style={{ background: colorBgContainer, }}/>
-        <Layout
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-            width: '75%'
-          }}
-        >
-          Content
-        </Layout>
+    // <Flex gap="middle" wrap>
+      <Layout>
+          <Header />
+        <Layout style={{ display: 'flex', flexDirection: 'row' }}>
+          <Sider style={{ background: 'white' }}/>
+          <Layout
+            style={{
+              margin: '16px 16px 0 16px',
+              padding: 24,
+              background: 'white',
+              borderRadius: 20,
+              width: '80%',
+            }}
+          >
+          <Text>Content</Text>
+          </Layout>
         </Layout>
       </Layout>
-    </Flex>
+    // </Flex>
   );
 };
 export default Main;
