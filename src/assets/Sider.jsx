33dd7c8de1f7c908cd5d2b042a41c5227332fa7a
iris5105 <iris510593@gmail.com>
2025-01-22 17:29:57 +0900

    화면 이동 방식 변경

--- src/assets/Sider.jsx
+++ src/assets/Sider.jsx
@@ -1,6 +1,121 @@
 import React from 'react';
 import { Layout, Menu} from 'antd';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { MailOutlined,AppstoreOutlined,SettingOutlined,LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import {useNavigate, Link } from 'react-router-dom';
+const items = [
+  {
+    key: '10',
+    label: '자문일일일',
+    icon: <MailOutlined />,
+    children: [
+                {
+                    key: '00',
+                    label: '시스템 수정 및 개발 의회',
+                    value :'Menu'
+                },
+                {
+                    type: 'divider'
+                },                
+                {
+                    key: '01',
+                    label: '회사 기본정보 관리',
+                    value : 'Menu1'
+                },
+                {
+                   type: 'divider'
+                },   
+                {
+                    key: '10',
+                    label: '계좌 관리그룹 등록',
+                    value : 'Menu2'
+                },
+                {
+                    key: '11',
+                    label: '계좌 계약정보 관리',
+                    value : 'Menu3'
+                },
+                {
+                    key: '12',
+                    label: '입,출금등록',
+                },
+                {
+                    type: 'divider'
+                },   
+                {
+                    key: '21',
+                    label: '현금 매입(종목)등록',
+                },
+                {
+                    key: '22',
+                    label: '체권 매입등록',
+                },
+            ],
+},
+  {
+    key: '20',
+    label: '공모청약(권리)',
+    icon: <AppstoreOutlined />,
+    children: [
+                {
+                    key: '5',
+                    label: 'Option 5',
+                },
+                {
+                    key: '6',
+                    label: 'Option 6',
+                },
+                {
+                key: '7',
+                label: 'Option 7',
+                },
+                {
+                key: '8',
+                label: 'Option 8',
+                },
+            ],
+  },
+  {
+    type: 'divider',
+  },
+  {
+    key: '30',
+    label: '자산운용용',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: '11',
+        label: 'Option 11',
+      },
+      {
+        key: '12',
+        label: 'Option 12',
+      },
+    ],
+  },
+  {
+    key: '40',
+    label: '외화자산운용',
+    type: 'group',
+    children: [
+      {
+        key: '13',
+        label: 'Option 13',
+      },
+      {
+        key: '14',
+        label: 'Option 14',
+      },
+    ],
+  },
+];
 const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
   const key = String(index + 1);
   return {
@@ -16,7 +131,15 @@ const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, i
     }),
   };
 });
+console.log(items2)
 export default function Sider(){
+    const navigate = useNavigate();  // useNavigate 훅 사용
+
+    const goMenu = ( item) => {
+      const value = item.props.value
+      // console.log('value : ', value.item.value)
+      navigate(value);  // navigate 함수로 '/Menu' 경로로 이동
+    };
   return (
     <Layout
           style={{
@@ -32,13 +155,14 @@ export default function Sider(){
     >
         <Menu
             mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
             style={{
             borderRight: 0,
             }}
-            items={items2}
+            items={items}
+            onSelect={(item) => goMenu(item.item)}
         />
+        <Link to=''></Link>
+        <button onClick={goMenu}>go menu</button>
     </Layout>
 
   )
