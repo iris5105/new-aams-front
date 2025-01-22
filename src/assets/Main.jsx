472991c76295f736cac91089782fd8a801497e2b
iris5105 <iris510593@gmail.com>
2025-01-22 17:29:45 +0900

    Main에서 모든 화면 불러오도록 템플릿 생성

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -1,35 +1,36 @@
 import React, {useState} from 'react';
 import { Layout, Typography, Splitter } from 'antd';
+import { Outlet } from 'react-router-dom';
 import Header from './Header.jsx';
 import Sider from './Sider.jsx';
 
+
 const {Text } = Typography
 const Main = () => {
-  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    // <Flex gap="middle" wrap>
-    <Layout>
-        <Header />
-        <Layout style={{ display: 'flex', flexDirection: 'row' }}>
-            <Splitter style={{height : 'auto'}}>
-                <Splitter.Panel defaultSize="20%" min="10%" max="70%">
-                    <Sider style={{ background: 'white' }}/>
-                </Splitter.Panel>
-                <Splitter.Panel 
-                    style={{
-                            margin: '16px',
-                            padding: 24,
-                            background: 'white',
-                            borderRadius: 20,
-                            width: '80%',
-                            }}
-                >
-                    <Text>Content</Text>
-                </Splitter.Panel>
-            </Splitter>
-        </Layout>
-    </Layout>
-    // </Flex>
+        <Layout>
+                <Header />
+                <Layout style={{ display: 'flex', flexDirection: 'row' }}>
+                  <Splitter style={{ height: 'auto' }}>
+                    <Splitter.Panel defaultSize="20%" min="10%" max="70%">
+                      <Sider style={{ background: 'white' }} />
+                    </Splitter.Panel>
+                    <Splitter.Panel
+                      style={{
+                        margin: '16px',
+                        padding: 24,
+                        background: 'white',
+                        borderRadius: 20,
+                        width: '80%',
+                      }}
+                    >
+
+                    <Outlet></Outlet>
+                    </Splitter.Panel>
+                  </Splitter>
+                </Layout>
+              </Layout>
   );
 };
 export default Main;
